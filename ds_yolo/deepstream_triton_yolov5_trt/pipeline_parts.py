96b089a73a89be34c17201b6c6f6a77996ac6ad7
D.N.QuAng <D.N.QuAng.Working@gmail.com>
2021-12-15 03:36:07 +0000

    rm skip_frames when encode and save video output

--- ds_yolo/deepstream_triton_yolov5_trt/pipeline_parts.py
+++ ds_yolo/deepstream_triton_yolov5_trt/pipeline_parts.py
@@ -78,61 +78,62 @@ class PipelineParts():
             except StopIteration:
                 break
             
-            if frame_meta.frame_num % 10 == 0:
-                frame_number = frame_meta.frame_num
-                num_rects = frame_meta.num_obj_meta
-                l_obj = frame_meta.obj_meta_list
-                while l_obj is not None:
-                    try:
-                        # Casting l_obj.data to pyds.NvDsObjectMeta
-                        obj_meta = pyds.NvDsObjectMeta.cast(l_obj.data)
-                    except StopIteration:
-                        break
-                    try:
-                        l_obj = l_obj.next
-                    except StopIteration:
-                        break
-
-                # Acquiring a display meta object. The memory ownership remains in
-                # the C code so downstream plugins can still access it. Otherwise
-                # the garbage collector will claim it when this probe function exits.
-                display_meta = pyds.nvds_acquire_display_meta_from_pool(batch_meta)
-                display_meta.num_labels = 1
-                py_nvosd_text_params = display_meta.text_params[0]
-                # Setting display text to be shown on screen
-                # Note that the pyds module allocates a buffer for the string, and the
-                # memory will not be claimed by the garbage collector.
-                # Reading the display_text field here will return the C address of the
-                # allocated string. Use pyds.get_string() to get the string content.
-
-                disp_string = "Frame Number={} Number of Objects={}"
-                
-                py_nvosd_text_params.display_text = disp_string.format(
-                    frame_number,
-                    num_rects,
-                )
-
-                # Now set the offsets where the string should appear
-                py_nvosd_text_params.x_offset = 10
-                py_nvosd_text_params.y_offset = 12
-
-                # Font , font-color and font-size
-                py_nvosd_text_params.font_params.font_name = "Serif"
-                py_nvosd_text_params.font_params.font_size = 10
-                # set(red, green, blue, alpha); set to White
-                py_nvosd_text_params.font_params.font_color.set(1.0, 1.0, 1.0, 1.0)
-
-                # Text background color
-                py_nvosd_text_params.set_bg_clr = 1
-                # set(red, green, blue, alpha); set to Black
-                py_nvosd_text_params.text_bg_clr.set(0.0, 0.0, 0.0, 1.0)
-                # Using pyds.get_string() to get display_text as string
-                print(pyds.get_string(py_nvosd_text_params.display_text))
-                pyds.nvds_add_display_meta_to_frame(frame_meta, display_meta)
+            #if frame_meta.frame_num % 10 == 0:
+            frame_number = frame_meta.frame_num
+            num_rects = frame_meta.num_obj_meta
+            l_obj = frame_meta.obj_meta_list
+            while l_obj is not None:
                 try:
-                    l_frame = l_frame.next
+                    # Casting l_obj.data to pyds.NvDsObjectMeta
+                    obj_meta = pyds.NvDsObjectMeta.cast(l_obj.data)
                 except StopIteration:
                     break
+                try:
+                    l_obj = l_obj.next
+                except StopIteration:
+                    break
+
+            # Acquiring a display meta object. The memory ownership remains in
+            # the C code so downstream plugins can still access it. Otherwise
+            # the garbage collector will claim it when this probe function exits.
+            display_meta = pyds.nvds_acquire_display_meta_from_pool(batch_meta)
+            display_meta.num_labels = 1
+            py_nvosd_text_params = display_meta.text_params[0]
+            # Setting display text to be shown on screen
+            # Note that the pyds module allocates a buffer for the string, and the
+            # memory will not be claimed by the garbage collector.
+            # Reading the display_text field here will return the C address of the
+            # allocated string. Use pyds.get_string() to get the string content.
+
+            disp_string = "Frame Number={} Number of Objects={}"
+            
+            py_nvosd_text_params.display_text = disp_string.format(
+                frame_number,
+                num_rects,
+            )
+
+            # Now set the offsets where the string should appear
+            py_nvosd_text_params.x_offset = 10
+            py_nvosd_text_params.y_offset = 12
+
+            # Font , font-color and font-size
+            py_nvosd_text_params.font_params.font_name = "Serif"
+            py_nvosd_text_params.font_params.font_size = 10
+            # set(red, green, blue, alpha); set to White
+            py_nvosd_text_params.font_params.font_color.set(1.0, 1.0, 1.0, 1.0)
+
+            # Text background color
+            py_nvosd_text_params.set_bg_clr = 1
+            # set(red, green, blue, alpha); set to Black
+            py_nvosd_text_params.text_bg_clr.set(0.0, 0.0, 0.0, 1.0)
+            # Using pyds.get_string() to get display_text as string
+            print(pyds.get_string(py_nvosd_text_params.display_text))
+            pyds.nvds_add_display_meta_to_frame(frame_meta, display_meta)
+        
+            try:
+                l_frame = l_frame.next
+            except StopIteration:
+                break
 
         return Gst.PadProbeReturn.OK
 
